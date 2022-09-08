83153145bd52caef4b648c895bca5c8d8873da65
OGrunner <orshagoines@gmail.com>
2022-09-07 22:47:21 -0400

    created index for models relationships

---
+++ index.js
@@ -0,0 +1,10 @@
+const Login = require("./Login");
+const Resume = require("./Resume");
+const JobSeeker = require("./JobSeeker");
+
+JobSeeker.HasMany(Resume);
+Resume.BelongsTo(JobSeeker);
+Login.BelongsTo(JobSeeker);
+JobSeeker.HasOne(Login);
+
+module.exports =  {JobSeeker, Resume, Login};
