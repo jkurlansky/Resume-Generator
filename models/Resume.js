9b5f120effcb53bf9a7042985ce8049895622048
Merkisha Wilkinson <merkisha94@gmail.com>
2022-09-09 15:52:40 -0400

    updated error

--- models/Resume.js
+++ models/Resume.js
@@ -10,27 +10,105 @@ Resume.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    jseeker_Id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Jobseeker,
+        key: "jseeker_Id",
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       validate: {
         isEmail: true,
       },
     },
-    objective: {
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    highlightedSkills: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    jobTitle1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    companyName1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    jobLocation1: { STRING, type: DataTypes.STRING, allowNull: false },
+    jobDescription1: { STRING, type: DataTypes.STRING, allowNull: false },
+    startDate1: { STRING, type: DataTypes.STRING, allowNull: false },
+    currentlyWorking1: { STRING, type: DataTypes.BOOLEAN },
+    endDate1: {
       type: DataTypes.STRING,
     },
-    workHistory: {
+    jobTitle2: { STRING, type: DataTypes.STRING },
+    companyName2: { STRING, type: DataTypes.STRING },
+    jobLocation2: { STRING, type: DataTypes.STRING },
+    jobDescription2: { STRING, type: DataTypes.STRING },
+    startDate2: { STRING, type: DataTypes.STRING, allowNull: false },
+    currentlyWorking2: { STRING, type: DataTypes.BOOLEAN },
+    endDate2: {
       type: DataTypes.STRING,
     },
-    education: {
+    jobTitle3: { STRING, type: DataTypes.STRING },
+    companyName3: { STRING, type: DataTypes.STRING },
+    jobLocation3: { STRING, type: DataTypes.STRING },
+    jobDescription3: { STRING, type: DataTypes.STRING },
+    startDate3: { STRING, type: DataTypes.STRING, allowNull: false },
+    currentlyWorking3: { STRING, type: DataTypes.BOOLEAN },
+    endDate3: {
       type: DataTypes.STRING,
     },
-    skills: {
+    degreeType1: {
       type: DataTypes.STRING,
     },
-    certifications: {
+    schoolName1: {
+      type: DataTypes.STRING,
+    },
+    eStartDate1: {
+      type: DataTypes.STRING,
+    },
+    eEndDate1: {
+      type: DataTypes.STRING,
+    },
+    degreeType2: {
+      type: DataTypes.STRING,
+    },
+    schoolName2: {
+      type: DataTypes.STRING,
+    },
+    eStartDate2: {
+      type: DataTypes.STRING,
+    },
+    eEndDate2: {
+      type: DataTypes.STRING,
+    },
+    certName1: {
+      type: DataTypes.STRING,
+    },
+    certOrg1: {
+      type: DataTypes.STRING,
+    },
+    certDate1: {
+      type: DataTypes.STRING,
+    },
+    certName2: {
+      type: DataTypes.STRING,
+    },
+    certOrg2: {
+      type: DataTypes.STRING,
+    },
+    certDate2: {
       type: DataTypes.STRING,
     },
   },
