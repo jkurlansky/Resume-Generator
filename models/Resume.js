51b1dee93078a075edae5f31fe5a472e3f9fbfab
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-09-08 21:25:43 -0400

    Updated resume.js

--- models/Resume.js
+++ models/Resume.js
@@ -17,29 +17,138 @@ Resume.init(
          key: 'jseeker_Id',
        },
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       validate: {
         isEmail: true,
       },
     },
-    objective: {
+    phoneNumber: {
         type: DataTypes.STRING,
+        allowNull: false
       },
-    workHistory: {
+    highlightedSkills: {
       type: DataTypes.STRING,
+      allowNull: false
     },
-    education:{
+    jobTitle1:{
       type: DatatTypes.STRING,
+      allowNull: false
     },
-    skills:{
+    companyName1:{
       type: DataTypes.STRING,
+      allowNull: false
       },
-    certifications: {STRING,
+    jobLocation1: {STRING,
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    jobDescription1: {STRING,
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    startDate1: {STRING,
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    currentlyWorking1: {STRING,
+      type: DataTypes.BOOLEAN,
+    },
+    endDate1: {
       type: DataTypes.STRING,
     },
+    jobTitle2: {STRING,
+      type: DataTypes.STRING,
+    },
+    companyName2: {STRING,
+      type: DataTypes.STRING,
+    },
+    jobLocation2: {STRING,
+      type: DataTypes.STRING,
+    },
+    jobDescription2: {STRING,
+      type: DataTypes.STRING,
+    },
+    startDate2: {STRING,
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    currentlyWorking2: {STRING,
+      type: DataTypes.BOOLEAN,
+    },
+    endDate2: {
+      type: DataTypes.STRING,
+    },
+    jobTitle3: {STRING,
+      type: DataTypes.STRING,
+    },
+    companyName3: {STRING,
+      type: DataTypes.STRING,
+    },
+    jobLocation3: {STRING,
+      type: DataTypes.STRING,
+    },
+    jobDescription3: {STRING,
+      type: DataTypes.STRING,
+    },
+    startDate3: {STRING,
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    currentlyWorking3: {STRING,
+      type: DataTypes.BOOLEAN,
+    },
+    endDate3: {
+      type: DataTypes.STRING,
+    },
+    degreeType1: {
+      type: DataTypes.STRING,
+    },
+    schoolName1: {
+      type: DataTypes.STRING,
+    },
+    eStartDate1: {
+      type: DataTypes.STRING,
+    },
+    eEndDate1: {
+      type: DataTypes.STRING,
+    },
+    degreeType2: {
+      type: DataTypes.STRING,
+    },
+    schoolName2: {
+      type: DataTypes.STRING,
+    },
+    eStartDate2: {
+      type: DataTypes.STRING,
+    },
+    eEndDate2: {
+      type: DataTypes.STRING,
+    },
+    certName1: {
+      type: DataTypes.STRING,
+    },
+    certOrg1: {
+      type: DataTypes.STRING,
+    },
+    certDate1: {
+      type: DataTypes.STRING,
+    },
+    certName2: {
+      type: DataTypes.STRING,
+    },
+    certOrg2: {
+      type: DataTypes.STRING,
+    },
+    certDate2: {
+      type: DataTypes.STRING,
+    },
+
   },
   {
     sequelize,
