6863ebee9f9535fa3440ef78ec6a08d1c47b7d98
Merkisha Wilkinson <merkisha94@gmail.com>
2022-09-13 19:21:00 -0400

    add css for login form

--- controllers/html/home.js
+++ controllers/html/home.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.render("home");
+  const loggedIn = req.session.loggedIn;
+  console.log("loggedIn", loggedIn);
+
+  res.render("home", {
+    loggedIn,
+  });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { layout: "main" });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("sign-up", { layout: "main" });
 });
 
 module.exports = router;
