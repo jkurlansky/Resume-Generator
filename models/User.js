6ecb5195ffbb150620804eae1aa00c01aa1793ab
jkurlansky <105459839+jkurlansky@users.noreply.github.com>
2022-09-15 20:48:15 -0400

    fixed sign up issue

--- models/User.js
+++ models/User.js
@@ -10,11 +10,6 @@ class User extends Model {
 
 User.init(
   {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
