f53b6a68b24bbb185865241682d0289aa2cc6f5d
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-09-16 16:40:15 -0400

    Added hide section functions to form.js

--- public/js/form.js
+++ public/js/form.js
@@ -10,14 +10,43 @@ const displayCert2 = document.getElementById("displayCert2");
 
 
 //Functions
-
-const formSubmit = async => {
-   
-
-
+function showJob2 () {
+    displayJob2.classList.remove("hide");
 }
 
+function showJob3 () {
+    displayJob3.classList.remove("hide");
+}
+
+function showEd1 () {
+    displayEd1.classList.remove("hide");
+}
+
+function showEd2 () {
+    displayEd2.classList.remove("hide");
+}
+
+function showCert1 () {
+    displayCert1.classList.remove("hide");
+}
+
+function showCert2 () {
+    displayCert2.classList.remove("hide");
+}
+
+// const formSubmit = async => {
+
+// }
+
 //Event Listeners
-document
-    .querySelector('#submitBtn')
-    .addEventListener("submit", )
+displayJob2.addEventListener("click", showJob2);
+displayJob3.addEventListener("click", showJob3);
+displayEd1.addEventListener("click", showEd1);
+displayEd2.addEventListener("click", showEd2);
+displayCert1.addEventListener("click", showCert1);
+displayCert2.addEventListener("click", showCert2);
+
+
+// document
+//     .querySelector('#submitBtn')
+//     .addEventListener("submit", )
