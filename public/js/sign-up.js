ab2daf59f132f925254d40216be8caf8e50b377b
Merkisha Wilkinson <merkisha94@gmail.com>
2022-09-15 20:08:37 -0400

    fixed signup route issue and display

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,14 +1,13 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  if (username && email && password) {
-    const response = await fetch("/api/users", {
+  if (email && password) {
+    const response = await fetch("/api/users/signup", {
       method: "POST",
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
 
