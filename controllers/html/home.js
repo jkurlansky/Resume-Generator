52cbfbb0f7d0280553f11ca5a807e4a63e79d0fe
jkurlansky <105459839+jkurlansky@users.noreply.github.com>
2022-09-16 21:40:55 -0400

    added query selectors for resume form

--- controllers/html/home.js
+++ controllers/html/home.js
@@ -32,7 +32,7 @@ router.get("/profile", withAuth, async (req, res) => {
 
     res.render("profile", {
       ...user,
-      logged_in: true,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
